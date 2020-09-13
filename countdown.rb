acd57d1ee7b07dde827766fc40f01efe2de631b7
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-13 04:48:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,9 @@
 #write your code here
 
 def countdown
-  number = 10
-    while number > 0
-    puts "#{number} SECOND(S)"
-    x -= 1
+  
+number = 10
+  while number > 0
+  puts "#{number} SECOND(S)"
+  x -= 1
 end
